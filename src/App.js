4aa2f1c12f8ab53e1484ba58346daf522eeef4de
Your Name <you@example.com>
2022-02-28 17:03:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState ,useEffect} from 'react'
+import axios from 'axios'
+export default function App() {
+  const [term ,setTerm] = useState('javascript')
+  const [debounceSearch ,setDebounceSearch] = useState(term)
+  const [result , setResult] = useState([])
 
-function App() {
+
+
+
+
+  useEffect(() => {
+    const timeOut = setTimeout(()=>setDebounceSearch(term),1000)
+    return()=>{
+      clearTimeout(timeOut)
+    }
+  }, [term])
+
+
+  useEffect(() => {
+       const search = async () =>{
+        const respond = await axios.get('https://en.wikipedia.org/w/api.php',{
+          params:{
+            action:'query',
+            list:'search',
+            origin:'*',
+            format:'json',
+            srsearch:debounceSearch,
+
+          },
+
+        })
+          setResult(respond.data.query.search)
+     }
+     search()
+  }, [debounceSearch])
+
+
+
+//   useEffect(()=>{
+  
+
+//      if (!result.length) {
+//           if(term){
+//          search()
+//         }
+//      }else{
+//  const debounceSearch = setTimeout (()=>{
+//          if(term){
+//          search()
+//      }
+//      },700)
+
+//      return()=>{
+//        clearTimeout(debounceSearch)
+//      }
+//      }
+    
+//   },[term,result.length])
+
+  const fetchResult = result.map((el)=>{
+    return( 
+             <tr  key={el.pageid}>
+                <td scope='row'>1</td>
+                <td>{el.title}</td>
+                <td>
+                <span dangerouslySetInnerHTML={{__html:el.snippet}}/>
+                </td>
+              </tr>
+              )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <div className='my-3'>
+            <label htmlFor='exampleFormControlInput1' className='form-label'>
+              Search Input
+            </label>
+            <input
+              type='text'
+              className='form-control'
+              id='exampleFormControlInput1'
+              onChange={(e)=>setTerm(e.target.value)}
+              value={term}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='col'>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Title</th>
+                <th scope='col'>Desc</th>
+              </tr>
+            </thead>
+            <tbody>
+              {fetchResult}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
